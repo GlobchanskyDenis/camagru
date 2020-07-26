9e2c7c404ebf4bf7ab842cf652a452140e60dced
Balgruuf Sabre <bsabre-c@oa-e1.msk.21-school.ru>
2020-07-26 14:23:44 +0300

    with test deploy

--- config/setup.sh
+++ config/setup.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-mysql -u root < setup.sql
+#mysql -u root < setup.sql
 php setup.php
+chmod 777 ../photoStorage
+## Рекурсивное копирование из текущей папки в папку на сервере
+#scp -r . Server:/home/bsabre/Documents/Camagru
